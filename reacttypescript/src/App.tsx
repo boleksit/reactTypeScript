fb41835495323a8c7c18acd30c9fa5c5d1afef22
Jakub Sitko <jakub.sitko@microsoft.wsei.edu.pl>
2022-11-12 13:04:53 +0100

    init

--- reacttypescript/src/App.tsx
+++ reacttypescript/src/App.tsx
@@ -1,9 +1,23 @@
-function App() {
-  return (
-    <div className="App">
-      a
-    </div>
-  );
-}
+//https://github.com/sowinskimateusz/materialy-frameworki
+import {FC} from 'react';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
-export default App;
+import { Home } from './components/Routes/Home/home';
+import { Posts } from './components/Routes/Posts/posts';
+
+export const App:FC=()=>{
+  return (
+    <BrowserRouter>
+      <img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt=""/>
+      <div>
+        <>{console.log('test')}</>
+        <Link to="posts">Posts</Link>
+        <Link to="/">Home</Link>
+      </div>
+      <Routes>
+        <Route path="posts" element={<Posts/>}/>
+        <Route index element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+  );
+};
---
+++ reacttypescript/src/components/Routes/Home/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {FC} from 'react';
+
+export const Home : FC=props=>{
+    return(
+        <>
+            sdadsdsas
+        </>
+    );
+}
